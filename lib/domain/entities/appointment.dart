4631c6ce8398b07b8a7414dc379c666afa04b1e8
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-05-13 22:35:10 -0400

    Agregado los getters, setters, patrón factory, underscores a las variables priv y code documentation en la clase concreta de appointment

--- lib/domain/entities/appointment.dart
+++ lib/domain/entities/appointment.dart
@@ -4,21 +4,55 @@ import '../enumerations/statusType_enum.dart';
 import '../valueobjects/appointment/appointment_date.dart';
 import '../valueobjects/appointment/appointment_id.dart';
 
+///Appointment: Es una clase concreta utilizada para el manejo de las citas.
 class Appointment {
-  AppointmentId id;
-  StatusType status;
-  AppointmentDate date;
-  AppointmentType appointmentType;
-  SpecialtyType specialtyType;
+  late final AppointmentId _id;
+  late StatusType _status;
+  late AppointmentDate _date;
+  late AppointmentType _appointmentType;
+  late SpecialtyType _specialtyType;
 
-  Appointment({
-    required this.id,
-    required this.status,
-    required this.date,
-    required this.appointmentType,
-    required this.specialtyType,
-  });
 
+  //Getters
+  AppointmentId get id => _id;
+  StatusType get status => _status;
+  AppointmentDate get date => _date;
+  AppointmentType get appointmentType => _appointmentType;
+  SpecialtyType get specialtyType => _specialtyType;
+
+  //Setters
+  set status(StatusType status) => _status = status;
+  set date(AppointmentDate date) => _date = date;
+  set appointmentType(AppointmentType appointmentType) => _appointmentType = appointmentType;
+  set specialtyType(SpecialtyType specialtyType) => _specialtyType = specialtyType;
+
+  //Constructor
+  Appointment(
+    AppointmentId id,
+    StatusType status,
+    AppointmentDate date,
+    AppointmentType appointmentType,
+    SpecialtyType specialtyType) {
+    _id = id;
+    _status = status;
+    _date = date;
+    _appointmentType = appointmentType;
+    _specialtyType = specialtyType;
+  }
+
+
+  ///Patron Factory.
+  static Appointment create(
+      AppointmentId id,
+      StatusType status,
+      AppointmentDate date,
+      AppointmentType appointmentType,
+      SpecialtyType specialtyType) {
+    return Appointment(id, status, date, appointmentType, specialtyType);
+  }
+
+
+  //Métodos
   void reschedule(AppointmentDate newDate) {}
 
   void updateStatus(StatusType newStatus) {}
