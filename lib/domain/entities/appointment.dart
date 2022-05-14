e6da3da35a62b387abf315b15ddc6fb02ba698cc
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-05-13 22:19:35 -0400

    Agregado el SpecialityType como atributo en la entidad appointment

--- lib/domain/entities/appointment.dart
+++ lib/domain/entities/appointment.dart
@@ -1,4 +1,5 @@
 import '../enumerations/appointmentType_enum.dart';
+import '../enumerations/specialty_type_enum.dart';
 import '../enumerations/statusType_enum.dart';
 import '../valueobjects/appointment/appointment_date.dart';
 import '../valueobjects/appointment/appointment_id.dart';
@@ -8,12 +9,14 @@ class Appointment {
   StatusType status;
   AppointmentDate date;
   AppointmentType appointmentType;
+  SpecialtyType specialtyType;
 
   Appointment({
     required this.id,
     required this.status,
     required this.date,
     required this.appointmentType,
+    required this.specialtyType,
   });
 
   void reschedule(AppointmentDate newDate) {}
