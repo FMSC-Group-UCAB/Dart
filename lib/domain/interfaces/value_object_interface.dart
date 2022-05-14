5aad9a89944f4589ad1faacd9808fbda73f0e425
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-05-14 13:05:40 -0400

    Mejorado el code doc de la interfaz value object

--- lib/domain/interfaces/value_object_interface.dart
+++ lib/domain/interfaces/value_object_interface.dart
@@ -1,4 +1,7 @@
-/// IValueObject: Es una interfaz que debe implementar cualquier clase que represente un Value Object.
+/// IValueObject: Es una interfaz genérica que debe implementar cualquier clase que represente un Value Object.
+/// [T] es el tipo de dato que representa el Value Object.
 abstract class IValueObject<T> {
+
+  ///Compara la igualdad de dos Value Objects.
   bool equals(T other);
 }
