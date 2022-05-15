10b4194c6e803e7fdaa22f0f194bac2b1008f7bd
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-05-15 09:57:40 -0400

    Cambiado el tipo de dato del PatientBirthDate de String a DateTime

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,13 @@
+import '../lib/domain/entities/doctor.dart';
+import '../lib/domain/entities/patient.dart';
 import '../lib/domain/enumerations/hold_type_enum.dart';
+import '../lib/domain/enumerations/specialty_type_enum.dart';
 import '../lib/domain/observables/domain_event.dart';
 import '../lib/domain/usecases/register_patient_usecase.dart';
+import '../lib/domain/valueobjects/doctor/doctor_first_name.dart';
+import '../lib/domain/valueobjects/doctor/doctor_id.dart';
+import '../lib/domain/valueobjects/doctor/doctor_last_name.dart';
+import '../lib/domain/valueobjects/doctor/doctor_location.dart';
 import '../lib/domain/valueobjects/patient/patient_id.dart';
 import '../lib/domain/valueobjects/patient/patient_firstName.dart';
 import '../lib/domain/valueobjects/patient/patient_lastName.dart';
@@ -12,6 +19,25 @@ import '../lib/domain/valueobjects/patient/patient_occupation.dart';
 void main(List<String> arguments) {
   print('Main');
 
+  final Doctor doctor = Doctor.create(
+    DoctorId.create(1), 
+    DoctorFirstName.create('María'),
+    DoctorLastName.create('Pérez'),
+    [SpecialtyType.CARDIOLOGY, SpecialtyType.OPHTALMOLOGY],
+    DoctorLocation('1', '-1'),
+    HoldType.NONE);
+
+  final Patient patient = Patient.create(
+    PatientId.create(1), 
+    PatientFirstName.create('Nicole'),
+    PatientLastName.create('Marcano'),
+    PatientBirthDate.create(DateTime.now()),
+    PatientEmail.create('nicole@gmail.com'),
+    PatientPhoneNumber.create('+58 (123)153-1532'),
+    PatientOccupation.create('Estudiante'),
+    HoldType.NONE
+  );
+
   DomainEvent("Nombre Cambiado", {});
 
 // Invocacion del Caso de Uso registrar paciente
@@ -20,9 +46,13 @@ void main(List<String> arguments) {
       PatientId(1),
       PatientFirstName('Froilan'),
       PatientLastName('Roa'),
-      PatientBirthDate('1998-17-4'),
+      PatientBirthDate(DateTime.now()),
       PatientEmail('email@example.com'),
       PatientPhoneNumber('+58 0000000000'),
       PatientOccupation('Student'),
       HoldType.NONE);
+
+  print('------------------------------------------------------------------');
+  //Caso de uso para solicitar una cita
+
 }
