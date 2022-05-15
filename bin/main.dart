5bce7afd965deda048c56457b89534283e6422b4
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-05-15 10:20:47 -0400

    Probado exitosamente el caso de uso request appointment en el main

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,15 @@
 import '../lib/domain/entities/doctor.dart';
 import '../lib/domain/entities/patient.dart';
+import '../lib/domain/enumerations/appointmentType_enum.dart';
 import '../lib/domain/enumerations/hold_type_enum.dart';
 import '../lib/domain/enumerations/specialty_type_enum.dart';
+import '../lib/domain/enumerations/statusType_enum.dart';
 import '../lib/domain/observables/domain_event.dart';
+import '../lib/domain/observables/observable.dart';
+import '../lib/domain/observables/observer.interface.dart';
 import '../lib/domain/usecases/register_patient_usecase.dart';
+import '../lib/domain/usecases/request_appointment_usecase.dart';
+import '../lib/domain/valueobjects/appointment/appointment_date.dart';
 import '../lib/domain/valueobjects/doctor/doctor_first_name.dart';
 import '../lib/domain/valueobjects/doctor/doctor_id.dart';
 import '../lib/domain/valueobjects/doctor/doctor_last_name.dart';
@@ -16,8 +22,17 @@ import '../lib/domain/valueobjects/patient/patient_email.dart';
 import '../lib/domain/valueobjects/patient/patient_phoneNumber.dart';
 import '../lib/domain/valueobjects/patient/patient_occupation.dart';
 
+
+class NuevoObservador extends Observer {
+  raise(List<DomainEvent> events) {
+    events.forEach((event) => print(event.toString()));
+  }
+}
+
 void main(List<String> arguments) {
   print('Main');
+
+  final Observer observer = NuevoObservador();
 
   final Doctor doctor = Doctor.create(
     DoctorId.create(1), 
@@ -54,5 +69,16 @@ void main(List<String> arguments) {
 
   print('------------------------------------------------------------------');
   //Caso de uso para solicitar una cita
+
+  final RequestAppointmentUseCase requestAppointment = RequestAppointmentUseCase();
+
+  requestAppointment.add(observer);
+
+  requestAppointment.requestAppointment(
+      patient,
+      doctor,
+      AppointmentDate.create(new DateTime(2023, 1, 1)),
+      AppointmentType.VIRTUAL,
+      SpecialtyType.OPHTALMOLOGY);
 
 }
