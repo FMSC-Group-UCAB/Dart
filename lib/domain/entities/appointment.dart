928fd4604dd2929e8b1d78f85f0d7a7d7eb1d6c7
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2022-05-14 23:33:19 -0400

    Agregacion de las relaciones entre objetos
    Agregacion de las relacion de Appointment con Doctor y paciente, CaseFile con Doctor y Paciente, y por ultimo Paciente con suscripcion

--- lib/domain/entities/appointment.dart
+++ lib/domain/entities/appointment.dart
@@ -3,31 +3,37 @@ import '../enumerations/specialty_type_enum.dart';
 import '../enumerations/statusType_enum.dart';
 import '../valueobjects/appointment/appointment_date.dart';
 import '../valueobjects/appointment/appointment_id.dart';
+import 'doctor.dart';
+import 'patient.dart';
 
 ///Appointment: Es una clase concreta utilizada para el manejo de las citas.
 class Appointment {
   late final AppointmentId _id;
+  late final Patient _patient;
+  late final Doctor _doctor;
   late StatusType _status;
   late AppointmentDate _date;
   late AppointmentType _appointmentType;
   late SpecialtyType _specialtyType;
 
-
   //Getters
   AppointmentId get id => _id;
+  Patient get patient => _patient;
+  Doctor get doctor => _doctor;
   StatusType get status => _status;
   AppointmentDate get date => _date;
   AppointmentType get appointmentType => _appointmentType;
   SpecialtyType get specialtyType => _specialtyType;
 
-
   //Constructor
   Appointment(
-    AppointmentId id,
-    StatusType status,
-    AppointmentDate date,
-    AppointmentType appointmentType,
-    SpecialtyType specialtyType) {
+      AppointmentId id,
+      Patient patient,
+      Doctor doctor,
+      StatusType status,
+      AppointmentDate date,
+      AppointmentType appointmentType,
+      SpecialtyType specialtyType) {
     _id = id;
     _status = status;
     _date = date;
@@ -35,25 +41,27 @@ class Appointment {
     _specialtyType = specialtyType;
   }
 
-
   ///Patron Factory.
   ///La funcion recibe [id], [status], [date], [appointmentType], [specialtyType]
   ///para crear una instancia de la clase [Appointment].
   static Appointment create(
       AppointmentId id,
+      Patient patient,
+      Doctor doctor,
       StatusType status,
       AppointmentDate date,
       AppointmentType appointmentType,
       SpecialtyType specialtyType) {
-    return Appointment(id, status, date, appointmentType, specialtyType);
+    return Appointment(
+        id, patient, doctor, status, date, appointmentType, specialtyType);
   }
 
-
   ///Métodos de la clase.
-  
+
   ///Esta función permite actualizar los datos de la cita, los cuales son:
   /// [status], [date], [appointmentType], [specialtyType]
-  void update(StatusType status, AppointmentDate date, AppointmentType appointmentType, SpecialtyType specialtyType) {
+  void update(StatusType status, AppointmentDate date,
+      AppointmentType appointmentType, SpecialtyType specialtyType) {
     _status = status;
     _date = date;
     _appointmentType = appointmentType;
@@ -61,5 +69,4 @@ class Appointment {
 
     //TODO: Agregar las validaciones de los campos.
   }
-
 }
