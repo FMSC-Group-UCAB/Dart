9af988426e818e1f30e3e8c73cbc3058dff8c222
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2022-05-15 20:56:53 -0400

    Se agrego Caso de Uso Registrar Suscr al main
    Se agrego el Caso de uso registrar suscripcion al main y se hizo unas modificaciones al caso de uso.

--- bin/main.dart
+++ bin/main.dart
@@ -4,10 +4,13 @@ import '../lib/domain/enumerations/appointmentType_enum.dart';
 import '../lib/domain/enumerations/hold_type_enum.dart';
 import '../lib/domain/enumerations/specialty_type_enum.dart';
 import '../lib/domain/enumerations/statusType_enum.dart';
+import '../lib/domain/enumerations/suscription_cost_type_enum.dart';
+import '../lib/domain/enumerations/suscription_type_enum.dart';
 import '../lib/domain/observables/domain_event.dart';
 import '../lib/domain/observables/observable.dart';
 import '../lib/domain/observables/observer.interface.dart';
 import '../lib/domain/usecases/register_patient_usecase.dart';
+import '../lib/domain/usecases/register_suscription_usecase.dart';
 import '../lib/domain/usecases/request_appointment_usecase.dart';
 import '../lib/domain/valueobjects/appointment/appointment_date.dart';
 import '../lib/domain/valueobjects/doctor/doctor_first_name.dart';
@@ -21,7 +24,20 @@ import '../lib/domain/valueobjects/patient/patient_birthDate.dart';
 import '../lib/domain/valueobjects/patient/patient_email.dart';
 import '../lib/domain/valueobjects/patient/patient_phoneNumber.dart';
 import '../lib/domain/valueobjects/patient/patient_occupation.dart';
+import '../lib/domain/interfaces/pay_method_interface.dart';
+import '../lib/domain/valueobjects/suscription/subscription_closed_at.dart';
+import '../lib/domain/valueobjects/suscription/subscription_created_at.dart';
+import '../lib/domain/valueobjects/suscription/subscription_id.dart';
+import '../lib/domain/valueobjects/suscription/subscription_paid_at.dart';
 
+///Clase prueba para poder instanciar la interfaz de metodos de pago
+class paypal implements IPayMethod {
+  @override
+  Future<bool> pay(double cost) async {
+    print('Pago exitoso');
+    return true;
+  }
+}
 
 class NuevoObservador extends Observer {
   raise(List<DomainEvent> events) {
@@ -35,23 +51,22 @@ void main(List<String> arguments) {
   final Observer observer = NuevoObservador();
 
   final Doctor doctor = Doctor.create(
-    DoctorId.create(1), 
-    DoctorFirstName.create('María'),
-    DoctorLastName.create('Pérez'),
-    [SpecialtyType.CARDIOLOGY, SpecialtyType.OPHTALMOLOGY],
-    DoctorLocation('1', '-1'),
-    HoldType.NONE);
+      DoctorId.create(1),
+      DoctorFirstName.create('María'),
+      DoctorLastName.create('Pérez'),
+      [SpecialtyType.CARDIOLOGY, SpecialtyType.OPHTALMOLOGY],
+      DoctorLocation('1', '-1'),
+      HoldType.NONE);
 
   final Patient patient = Patient.create(
-    PatientId.create(1), 
-    PatientFirstName.create('Nicole'),
-    PatientLastName.create('Marcano'),
-    PatientBirthDate.create(DateTime.now()),
-    PatientEmail.create('nicole@gmail.com'),
-    PatientPhoneNumber.create('+58 (123)153-1532'),
-    PatientOccupation.create('Estudiante'),
-    HoldType.NONE
-  );
+      PatientId.create(1),
+      PatientFirstName.create('Nicole'),
+      PatientLastName.create('Marcano'),
+      PatientBirthDate.create(DateTime.now()),
+      PatientEmail.create('nicole@gmail.com'),
+      PatientPhoneNumber.create('+58 (123)153-1532'),
+      PatientOccupation.create('Estudiante'),
+      HoldType.NONE);
 
   DomainEvent("Nombre Cambiado", {});
 
@@ -70,7 +85,8 @@ void main(List<String> arguments) {
   print('------------------------------------------------------------------');
   //Caso de uso para solicitar una cita
 
-  final RequestAppointmentUseCase requestAppointment = RequestAppointmentUseCase();
+  final RequestAppointmentUseCase requestAppointment =
+      RequestAppointmentUseCase();
 
   requestAppointment.add(observer);
 
@@ -81,4 +97,29 @@ void main(List<String> arguments) {
       AppointmentType.VIRTUAL,
       SpecialtyType.OPHTALMOLOGY);
 
+  print('------------------------------------------------------------------');
+
+  ///Crear un paciente prueba
+  final patientTest = Patient.create(
+      PatientId(1),
+      PatientFirstName('Froilan'),
+      PatientLastName('Roa'),
+      PatientBirthDate(DateTime.utc(1989, 1, 1)),
+      PatientEmail('email@email.com'),
+      PatientPhoneNumber('+58 0000000000'),
+      PatientOccupation('Student'),
+      HoldType.NONE);
+
+  // Invocacion del Caso de Uso registrar suscripcion
+  final RegisterSuscriptionUsecase registersubscription =
+      RegisterSuscriptionUsecase(paypal());
+
+  registersubscription.registerSuscription(
+      SubscriptionId(1),
+      patientTest,
+      SubscriptionCreatedAt(DateTime.now()),
+      SubscriptionPaidAt(DateTime.now()),
+      SubscriptionClosedAt(DateTime.now()),
+      SuscriptionCostType.BASIC,
+      SuscriptionType.MONTHLY);
 }
